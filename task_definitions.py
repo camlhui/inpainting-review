5d6cb709feeb40f04717701dde422731da74ccf1
Camille L'Huillier <camille.lhuillier@pricehubble.com>
2024-11-26 22:51:31 +0100

    refactor workflow

---
+++ task_definitions.py
@@ -0,0 +1,16 @@
+from enum import Enum
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class Models(Enum):
+    FLUX_1_FILL_DEV = "black-forest-labs/FLUX.1-Fill-dev"
+
+
+class InpaintingTask(BaseModel):
+    task_id: str
+    source_image: str
+    mask_image: str
+    prompt: str
+    negative_prompt: Optional[str] = None
