ed4b301ce21d9955d5d5278a72509244dd93deb9
Camille L'Huillier <camille.lhuillier@pricehubble.com>
2024-11-26 20:08:26 +0100

    remove set_set_hugging_face_hub_token

--- workflows/utils.py
+++ workflows/utils.py
@@ -1,15 +1,3 @@
-import os
-
-
-def set_hugging_face_hub_token():
-    from google.colab import userdata
-
-    hf_hub_token = userdata.get("HUGGING_FACE_HUB_TOKEN")
-    assert hf_hub_token, "missing Google colab secret HUGGING_FACE_HUB_TOKEN"
-
-    os.environ["HUGGING_FACE_HUB_TOKEN"] = hf_hub_token
-
-
 def assert_cuda_availability():
     import torch
 
