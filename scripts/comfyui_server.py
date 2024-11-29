b1224739499593ea1dc3c0fa30ffdb0e2bde56f4
Camille L'Huillier <camille.lhuillier@pricehubble.com>
2024-11-29 18:36:08 +0100

    add comfyui server script

---
+++ scripts/comfyui_server.py
@@ -0,0 +1,106 @@
+import os
+import subprocess
+import tempfile
+
+
+PIP_DEPENDENCIES = dependencies = [
+    "accelerate",
+    "einops",
+    "transformers>=4.28.1",
+    "safetensors>=0.4.2",
+    "aiohttp",
+    "pyyaml",
+    "Pillow",
+    "scipy",
+    "tqdm",
+    "psutil",
+    "tokenizers>=0.13.3",
+    "torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121",
+    "torchsde",
+    "kornia>=0.7.1",
+    "spandrel",
+    "soundfile",
+    "sentencepiece",
+]
+
+
+def _install_pip_dependencies():
+    print("Installing pip dependencies")
+
+    for dependency in dependencies:
+        try:
+            subprocess.check_call(["pip3", "install", dependency])
+        except subprocess.CalledProcessError as e:
+            print(f"Failed to install {dependency}: {e}")
+            raise
+
+
+def _print_and_cleanup_logs(stdout_file: str, stderr_file: str):
+    print("\n--- stdout ---")
+    with open(stdout_file, "r") as f:
+        print(f.read())
+
+    print("\n--- stderr ---")
+    with open(stderr_file, "r") as f:
+        print(f.read())
+
+    os.remove(stdout_file)
+    os.remove(stderr_file)
+    print("\nTemporary log files have been deleted.")
+
+
+def start_comfyui_server():
+    _install_pip_dependencies()
+
+    stdout_file = tempfile.NamedTemporaryFile(
+        delete=False, mode="w+", suffix=".log", prefix="comfyui_stdout_"
+    )
+    stderr_file = tempfile.NamedTemporaryFile(
+        delete=False, mode="w+", suffix=".log", prefix="comfyui_stderr_"
+    )
+
+    assert os.environ.get(
+        ["COMFYUI_INSTALL_DIR"]
+    ), "Missing environment variable COMFYUI_INSTALL_DIR"
+
+    try:
+        process = subprocess.Popen(
+            [
+                "python",
+                os.path.join(os.environ.get(["COMFYUI_INSTALL_DIR"]), "main.py"),
+            ],
+            stdout=stdout_file,
+            stderr=stderr_file,
+        )
+
+        print(f"ComfyUI server started with PID {process.pid}")
+        print(f"stdout redirected to: {stdout_file.name}")
+        print(f"stderr redirected to: {stderr_file.name}")
+
+        return process, stdout_file.name, stderr_file.name
+
+    except Exception as e:
+        print(f"Failed to start ComfyUI server: {e}")
+        print(f"Failed to start ComfyUI server: {e}")
+        print(
+            f"Attempting to print and delete logs from:\nstdout: {stdout_file.name}\
+            \nstderr: {stderr_file.name}\n"
+        )
+        _print_and_cleanup_logs(stdout_file.name, stderr_file.name)
+        raise
+
+
+def stop_comfyui_server(process: subprocess.Popen, stdout_file: str, stderr_file: str):
+    if process and process.poll() is None:
+        print("Stopping the server...")
+        process.terminate()
+        process.wait()
+        print("Server stopped.")
+
+        print(
+            f"Attempting to print and delete logs from:\nstdout: {stdout_file}\
+            \nstderr: {stderr_file}\n"
+        )
+        _print_and_cleanup_logs(stdout_file, stderr_file)
+    else:
+        print("Server is not running or already stopped.")
